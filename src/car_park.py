622c64b165fba71903ad859a1a25726478c3032a
20143871 <20012345@tafe.wa.edu.au>
2025-10-29 10:44:46 +0800

    feat: set up CarPark attributes and str method

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,11 @@
 class CarPark:
-    pass
+    def __init__(self, location = "Unknown", capacity=0, plates = None, displays = None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or [] # uses the first value if not None, otherwise uses the second value
+        self.displays = displays or []
+        self.bays_availability = []
+        self.bays_availability += [True] * self.capacity # if a bay is `True` it means it is available
+
+    def __str__(self):
+        return f"Car park at {self.location}, with {self.capacity} bays."
